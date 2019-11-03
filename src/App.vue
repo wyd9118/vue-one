5175c28ea98b110709cfcd308f3360082f775c75
mypc <1016388013@qq.com>
2019-11-03 22:55:00 +0800

    'vue-one'

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Login from './components/Login'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="login-section">
+        <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="用户名：">
+                <el-input
+                placeholder="输入用户名" 
+                v-model="form.username"> 
+                </el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input
+                placeholder="输入密码" 
+                v-model="form.psw"> 
+                </el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button class="primary" @click="submitLogin">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+<script>
+    export default {
+        name:'login',
+        data(){
+            return {
+                form:{
+                    username:'',
+                    psw:'',
+                }
+            }
+        },
+        methods:{
+            submitLogin(){
+                
+            }
+        }
+    }
+</script>
+<style scoped>
+    .login-section{
+        width:200px;
+        margin:0 auto;
+        text-align:left;
+    }
+    .el-input {
+        display:block;
+    }
+</style>
