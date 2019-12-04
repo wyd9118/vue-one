6796c3a15633c4b7a3c4b3a274af9aca36debefa
wyd <wyd8729@163.com>
2019-12-04 18:20:54 +0800

    '20191204'

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,26 +1,19 @@
 <template>
-    <div class="login-section">
-        <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item label="用户名：">
-                <el-input
-                placeholder="输入用户名" 
-                v-model="form.username"> 
-                </el-input>
-            </el-form-item>
-            <el-form-item label="密码">
-                <el-input
-                placeholder="输入密码" 
-                v-model="form.psw"> 
-                </el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button class="primary" @click="submitLogin">登录</el-button>
-            </el-form-item>
-        </el-form>
-    </div>
+  <div class="login-section">
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="用户名：">
+        <el-input placeholder="输入用户名" v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input placeholder="输入密码" v-model="form.psw"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="primary" @click="submitLogin">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script>
-import qs from 'qs'
     export default {
         name:'login',
         data(){
@@ -50,20 +43,20 @@ import qs from 'qs'
                     data: d
                 })
                 .then(function(response){
-                    console.log(response)
-                };
+                    // console.log(response);
+                };    
 
             }
         }
     }
 </script>
 <style scoped>
-    .login-section{
-        width:200px;
-        margin:0 auto;
-        text-align:left;
-    }
-    .el-input {
-        display:block;
-    }
+.login-section {
+  width: 200px;
+  margin: 0 auto;
+  text-align: left;
+}
+.el-input {
+  display: block;
+}
 </style>
